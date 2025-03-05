ce7d03dce8d7e04deb0edb8f7037bc1c12846d12
Enzo Oliveira <eoliveira.devmaua@gmail.com>
2025-03-05 13:49:33 -0300

    Repository update

--- src/shared/domain/repositories/challenge_repository_interface.py
+++ src/shared/domain/repositories/challenge_repository_interface.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Optional
+from datetime import datetime, time
 
 from src.shared.domain.entities import Purchase
 
@@ -8,14 +9,14 @@ from src.shared.domain.entities import Purchase
 class IChallengeRepository(ABC):
 
     @abstractmethod
-    def post_new_purchase(self, purchase: Purchase) -> Purchase:
+    def create_purchase(self, purchase: Purchase) -> Purchase:
         pass
 
     @abstractmethod
-    def get_purchase_by_user_id(self, purchase: Purchase) -> List[str]:
+    def get_category_from_user_id(self, user_id: str) -> Optional[List[str]]:
         pass
 
     @abstractmethod
-    def get_purchase_by_user_id_in_interval(self, purchase: Purchase) -> List[str]:
+    def get_purchase_from_user_id_and_interval(self,user_id: str, start_date: int, end_date: int) -> Optional[List[str]]:
         pass
 
