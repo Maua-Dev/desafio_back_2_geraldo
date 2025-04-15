defefbc77f22af2a34e107759231fffee7b0bb0d
Pistori1 <eoliveira.devmaua@gmail.com>
2025-04-15 19:34:41 -0300

    Test repository mock fix

--- tests/src/shared/domain/repositories/test_purchase_repository_mock.py
+++ tests/src/shared/domain/repositories/test_purchase_repository_mock.py
@@ -1,9 +1,19 @@
 import pytest
 
+from src.shared.domain.entities.purchase import Purchase
 from src.shared.domain.enums.category_enum import CATEGORY
 from src.shared.domain.repositories.purchase_repository_mock import PurchaseRepositoryMock
 
 class Test_purchase_repository_mock:
+
+    def test_create_purchase(self):
+        repo = PurchaseRepositoryMock()
+
+        purchase = Purchase(user_id="e52bbdb7-aaa9-49a8-89be-f4ffd4bdc930", product_id="832ddff6-b83e-4759-ba88-0f683ac70fa3", 
+                       category=CATEGORY.CLOTHES, price= 27.0, purchase_date= 1744637400)
+        result = repo.create_purchase(purchase)
+
+        assert repo.purchase_list[-1] == purchase
 
     def test_get_category_from_user_id(self):
         repo = PurchaseRepositoryMock()
