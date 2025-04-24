789bef5036cbe6dd2ab0fe39f3907168d73133b4
Pistori1 <eoliveira.devmaua@gmail.com>
2025-04-24 20:39:40 -0300

    Controller padronized errors

--- src/modules/purchase/app/purchase_controller.py
+++ src/modules/purchase/app/purchase_controller.py
@@ -1,16 +1,19 @@
-from src.shared.helpers.external_interfaces.http_models import LambdaHttpRequest, LambdaHttpResponse
 from src.shared.helpers.errors.usecase_errors import DuplicatedItem
 from src.shared.helpers.errors.domain_errors import EntityError
 from src.shared.helpers.errors.usecase_errors import ForbiddenAction
 from src.modules.purchase.app.purchase_usecase import PurchaseUsecase
 from src.modules.purchase.app.purchase_viewmodel import PurchaseViewmodel
+from src.shared.helpers.external_interfaces.http_models import HttpRequest, HttpResponse
+from src.shared.helpers.external_interfaces.http_codes import BadRequest, Conflict, Created, Forbidden, InternalServerError, NotFound
+
+
 
 class PurchaseController:
 
     def __init__(self, usecase: PurchaseUsecase):
         self.usecase = usecase
 
-    def __call__(self, request: LambdaHttpRequest) -> LambdaHttpResponse:
+    def __call__(self, request: HttpRequest) -> HttpResponse:
 
         try:
             user_id = request.data.get("user_id")
@@ -28,13 +31,13 @@ class PurchaseController:
             )
 
             viewmodel = PurchaseViewmodel(purchase)
-            return LambdaHttpResponse(status_code=200, body=viewmodel.to_dict())
+            return Created(viewmodel.to_dict())
 
         except DuplicatedItem as error:
-            return LambdaHttpResponse(status_code=409, body={"message": str(error)})    
+            return Conflict(body=error.message)    
 
         except (EntityError, ForbiddenAction) as error:
-            return LambdaHttpResponse(status_code=400, body={"message": str(error)})
+            return Forbidden(body=error.message)
 
         except Exception as error:
-            return LambdaHttpResponse(status_code=500, body={"message": "Internal server error", "error": str(error)})
+            return InternalServerError(body=error.args[0])
---
+++ tests/modules/purchase/app/test_purchase_controller.py
@@ -0,0 +1,36 @@
+from src.modules.purchase.app.purchase_controller import PurchaseController
+from src.modules.purchase.app.purchase_usecase import PurchaseUsecase
+from src.shared.domain.enums.category_enum import CATEGORY
+from src.shared.domain.repositories.purchase_repository_mock import PurchaseRepositoryMock
+from src.shared.helpers.external_interfaces.http_models import HttpRequest
+
+
+
+class Test_PurchaseController:
+
+    def test_purchase_controller(self):
+        repo = PurchaseRepositoryMock()
+        usecase = PurchaseUsecase(repo=repo)
+        controller = PurchaseController(usecase=usecase)
+
+        request = HttpRequest(
+            body={
+                "user_id": "3c0c67a5-2dc4-4b90-be13-73b7d29718f0",
+                "product_id": "b5466cbf-581e-4f65-885f-247d03c96602",
+                "category": CATEGORY.CLOTHES,
+                "price": 50.0,
+                "purchase_date": 1744644600
+            },
+            headers={
+                "requester_user": {
+                    "user_id": "3c0c67a5-2dc4-4b90-be13-73b7d29718f0"
+                }
+            }
+        )
+
+        response = controller(request)
+
+        assert response.status_code == 201
+        assert response.body["message"] == "the purchase has been created"
+        assert response.body["purchase"]["price"] == 50.0
+
